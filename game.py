754188914891120c035b342294545e83d08531ce
Elena Anch <107437613+lenkaanch@users.noreply.github.com>
2026-02-07 01:52:08 +0300

    add main game loop

--- game.py
+++ game.py
@@ -14,3 +14,18 @@ def check_guess(secret, guess):
     return secret == guess
 
 print("Welcome to Guess the Number!")
+import random
+
+def generate_number():
+    return random.randint(1, 10)
+
+def check_guess(secret, guess):
+    return secret == guess
+
+secret = generate_number()
+guess = int(input("Guess a number from 1 to 10: "))
+
+if check_guess(secret, guess):
+    print("You win!")
+else:
+    print(f"You lose! The number was {secret}")
