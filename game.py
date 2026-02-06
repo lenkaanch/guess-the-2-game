22a93c3ff49bbb5e9f15b90eefbdeddf61fe477f
Elena Anch <107437613+lenkaanch@users.noreply.github.com>
2026-02-07 01:54:09 +0300

    refactor game logic into function

--- game.py
+++ game.py
@@ -29,3 +29,24 @@ if check_guess(secret, guess):
     print("You win!")
 else:
     print(f"You lose! The number was {secret}")
+
+import random
+
+def generate_number():
+    return random.randint(1, 10)
+
+def check_guess(secret, guess):
+    return secret == guess
+
+def play_game():
+    secret = generate_number()
+    guess = int(input("Guess a number from 1 to 10: "))
+
+    if check_guess(secret, guess):
+        print("You win!")
+    else:
+        print(f"You lose! The number was {secret}")
+
+if __name__ == "__main__":
+    print("Welcome to Guess the Number!")
+    play_game()
